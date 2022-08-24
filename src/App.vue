3e1f84d66c4e52940a04e1256559b842815b7674
Taufik Ibrahim <ana3abeetawy@gmail.com>
2022-08-24 23:49:20 +0200

    project starter

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<NavBar/>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue"
+export default {
+  name : "App",
+  components: {
+    NavBar
+  }
+}
+
---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Products</a>
+          <a class="nav-link" href="#">About Us</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+<script>
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,300&display=swap');
+
+a.navbar-brand {
+    font-family: 'Lato', sans-serif;
+}
+</style>
