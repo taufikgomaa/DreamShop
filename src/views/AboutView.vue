61817841626aeed66cd04f6ea39e8c827cce4f62
Taufik Ibrahim <ana3abeetawy@gmail.com>
2022-09-03 22:46:03 +0200

    few changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,165 @@
 <template>
-  <h1>this is about</h1>
+  <header class="masthead text-center text-white">
+    <div class="masthead-content">
+      <div class="container px-5">
+        <h1 class="masthead-heading mb-0">We Sell Dreams</h1>
+        <h2 class="masthead-subheading mb-0">Join Us</h2>
+        <router-link to="/products" class="btn btn-primary btn-xl rounded-pill mt-5"
+                >Products</router-link
+              >
+      </div>
+    </div>
+    <div class="bg-circle-1 bg-circle"></div>
+    <div class="bg-circle-2 bg-circle"></div>
+    <div class="bg-circle-3 bg-circle"></div>
+    <div class="bg-circle-4 bg-circle"></div>
+  </header>
+  <!-- Content section 1-->
+  <section id="scroll">
+    <div class="container px-5">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-6 order-lg-2">
+          <div class="p-5">
+            <img
+              class="img-fluid rounded-circle"
+              src="https://images.pexels.com/photos/11924729/pexels-photo-11924729.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="..."
+            />
+          </div>
+        </div>
+        <div class="col-lg-6 order-lg-1">
+          <div class="p-5">
+            <h2 class="display-4">Been In Service For A Long Time</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+              aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam
+              exercitationem, ea animi blanditiis recusandae! Ratione voluptatum
+              molestiae adipisci, beatae obcaecati.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Content section 2-->
+  <section>
+    <div class="container px-5">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-6">
+          <div class="p-5">
+            <img
+              class="img-fluid rounded-circle"
+              src="https://images.pexels.com/photos/8356962/pexels-photo-8356962.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="..."
+            />
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="p-5">
+            <h2 class="display-4">We Have A Great Work Culture</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+              aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam
+              exercitationem, ea animi blanditiis recusandae! Ratione voluptatum
+              molestiae adipisci, beatae obcaecati.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Content section 3-->
+  <section>
+    <div class="container px-5">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-6 order-lg-2">
+          <div class="p-5">
+            <img
+              class="img-fluid rounded-circle"
+              src="https://images.pexels.com/photos/13070737/pexels-photo-13070737.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="..."
+            />
+          </div>
+        </div>
+        <div class="col-lg-6 order-lg-1">
+          <div class="p-5">
+            <h2 class="display-4">Our Family Welcomes You</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+              aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam
+              exercitationem, ea animi blanditiis recusandae! Ratione voluptatum
+              molestiae adipisci, beatae obcaecati.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<style scoped>
+header.masthead {
+  position: relative;
+  overflow: hidden;
+  padding-top: calc(7rem + 72px);
+  padding-bottom: 7rem;
+  background: linear-gradient(0deg, #ff6a00 0%, #ee0979 100%);
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-attachment: scroll;
+  background-size: cover;
+}
+header.masthead .masthead-content {
+  z-index: 1;
+  position: relative;
+}
+header.masthead .masthead-content .masthead-heading {
+  font-size: 4rem;
+}
+header.masthead .masthead-content .masthead-subheading {
+  font-size: 2rem;
+}
+header.masthead .bg-circle {
+  z-index: 0;
+  position: absolute;
+  border-radius: 100%;
+  background: linear-gradient(0deg, #ee0979 0%, #ff6a00 100%);
+}
+header.masthead .bg-circle-1 {
+  height: 90rem;
+  width: 90rem;
+  bottom: -55rem;
+  left: -55rem;
+}
+header.masthead .bg-circle-2 {
+  height: 50rem;
+  width: 50rem;
+  top: -25rem;
+  right: -25rem;
+}
+header.masthead .bg-circle-3 {
+  height: 20rem;
+  width: 20rem;
+  bottom: -10rem;
+  right: 5%;
+}
+header.masthead .bg-circle-4 {
+  height: 30rem;
+  width: 30rem;
+  top: -5rem;
+  right: 35%;
+}
+
+@media (min-width: 992px) {
+  header.masthead {
+    padding-top: calc(10rem + 55px);
+    padding-bottom: 10rem;
+  }
+  header.masthead .masthead-content .masthead-heading {
+    font-size: 6rem;
+  }
+  header.masthead .masthead-content .masthead-subheading {
+    font-size: 4rem;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,38 +1,143 @@
 <template>
   <section class="bg-image text-white">
     <p class="h1 shop-header-text shadow-lg">
-      Make Your <span class="text-danger">Dream</span> Come True With <span class="text-warning">Us</span>
+      Make Your <span class="text-danger">Dream</span> Come True With
+      <span class="text-warning">Us</span>
     </p>
     <p class="h3 shop-header-text shadow-lg">All Your Needs Is Here</p>
   </section>
-
-  <section class="container">
+  <p class="h1 text-center my-5">What We Have</p>
+  <section class="container my-5">
+    <div class="row">
+      <div class="col">
+        <div class="card shadow-lg" style="width: 18rem">
+          <img
+            src="https://i.gyazo.com/2abee4c1e5e17f88eed75a3c848f9111.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <p class="card-text">Get the latest tech.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-lg" style="width: 18rem">
+          <img
+            src="https://i.gyazo.com/005cc5899fbf0e6f1a922aa3ea2758d6.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Get well crafted jewels.<br />From all over the globe.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-lg" style="width: 18rem">
+          <img
+            src="https://i.gyazo.com/506255f2a442f8de02f4042b3e782b0d.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Get the most stylish designer clothes out there.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <p class="h1 text-center my-5">Great Service</p>
+  <section class="container my-5">
     <div class="row text-center">
       <div class="col">
-        lol
+        <p class="h3"><i class="fa-solid fa-truck"></i> Fast Delivery</p>
+        <p>Guaranteed wherever you live</p>
       </div>
+
       <div class="col">
-        lol
+        <p class="h3">
+          <i class="fa-sharp fa-solid fa-thumbs-up"></i> High Quality
+        </p>
+        <p>We believe you deserve better</p>
       </div>
+
       <div class="col">
-        lol
+        <p class="h3">
+          <i class="fa-solid fa-phone-volume"></i> Customer Support
+        </p>
+        <p>We have a lot of people willing to help you</p>
       </div>
+    </div>
+  </section>
+
+  <div class="bg-danger d-flex justify-content-around my-5">
+    <p class="text-white h4 my-5">Start you Journey With Us</p>
+    <router-link to="/products" class="btn bg-light h4 my-5"
+      >Get Started</router-link
+    >
+  </div>
+  <p class="h1 text-center my-5">Contact Us</p>
+  <section class="row my-5 container">
+    <div class="col">
+      <img src="https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=".." class="img-fluid">
+    </div>
+    <div class="col">
+      <form>
+        <div class="form-group">
+          <label for="user-name">User Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="user-name"
+            placeholder="User Name"
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlInput1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">Textarea</label>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
   </section>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
-  name: 'HomeView',
-  components: {
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap");
 
 .bg-image {
   background-image: url("https://i.gyazo.com/b29a2b85942a57d8f1da9e9b6b681bbc.jpg");
@@ -40,14 +145,14 @@ export default {
   background-size: cover;
   height: 400px;
   display: flex;
-  gap:20px;
+  gap: 20px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 }
 
 .shop-header-text {
-  font-family: 'Montserrat', sans-serif;
-  background: rgba(0,0,0,.8);
+  font-family: "Montserrat", sans-serif;
+  background: rgba(0, 0, 0, 0.8);
 }
 </style>
