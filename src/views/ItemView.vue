b2936c2cdfc62dd40b8e3f30953310fc7d639a8b
Taufik Ibrahim <ana3abeetawy@gmail.com>
2022-09-06 11:26:58 +0200

    final 3

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,46 +1,59 @@
 <template>
-    <section class="py-5">
-            <div class="container px-4 px-lg-5 my-5">
-                <div class="row gx-4 gx-lg-5 align-items-center">
-                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" :src="$store.state.itemPreview.image" alt="..." /></div>
-                    <div class="col-md-6">
-                        <div class="small mb-1">{{$store.state.itemPreview.id}}</div>
-                        <h1 class="display-5 fw-bolder">{{$store.state.itemPreview.title}}</h1>
-                        <div class="fs-5 mb-5">
-                            <span>${{$store.state.itemPreview.price}}</span>
-                        </div>
-                        <p class="lead">{{$store.state.itemPreview.description}}</p>
-                        <div class="d-flex">
-                            <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
-                            <button class="btn btn-outline-dark flex-shrink-0" type="button" @click="$store.commit('addToCart', item)">
-                                <i class="bi-cart-fill me-1"></i>
-                                Add to cart
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+  <section class="py-5">
+    <div class="container px-4 px-lg-5 my-5">
+      <div class="row gx-4 gx-lg-5 align-items-center">
+        <div class="col-md-6">
+          <img
+            class="card-img-top mb-5 mb-md-0"
+            :src="$store.state.itemPreview.image"
+            alt="..."
+          />
+        </div>
+        <div class="col-md-6">
+          <div class="small mb-1">{{ $store.state.itemPreview.id }}</div>
+          <h1 class="display-5 fw-bolder">
+            {{ $store.state.itemPreview.title }}
+          </h1>
+          <div class="fs-5 mb-5">
+            <span>${{ $store.state.itemPreview.price }}</span>
+          </div>
+          <p class="lead">{{ $store.state.itemPreview.description }}</p>
+          <div class="d-flex">
+            <router-link
+              to="/cart"
+              class="btn btn-outline-dark flex-shrink-0"
+              @click="$store.commit('addToCart', item)"
+            >
+              <i class="bi-cart-fill me-1"></i>
+              Add to cart
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    name: "ItemView",
-    data() {
-        return {
-            item: {}
-        }
-    },
-    async created() {
-        const res = await fetch(`http://localhost:3000/products/${this.$store.state.itemPreview.id}`);
-        this.item = await res.json();
-    }
-}
+  name: "ItemView",
+  data() {
+    return {
+      item: {},
+    };
+  },
+  async created() {
+    const res = await fetch(
+      `http://localhost:3000/products/${this.$store.state.itemPreview.id}`
+    );
+    this.item = await res.json();
+  },
+};
 </script>
 
 <style scoped>
 .card-img-top {
-    width: 600px;
-    height: 700px;
+  width: 600px;
+  height: 700px;
 }
 </style>
